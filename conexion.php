01b77bd868a64ba85b19a96df5c761448b17fd79
sofiarizzo04 <sofirizzo412@gmail.com>
2026-07-01 12:06:03 -0300

    conexion a la base de datos

--- conexion.php
+++ conexion.php
@@ -1,6 +1,6 @@
 <?php
 
-$conexion = new mysqli("localhost", "root", "", "empresa");
+$conexion = new mysqli("localhost", "root", "", "login");
 
 if ($conexion->connect_error) {
     die("Error de conexión");
