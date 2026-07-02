6dd54acf4c030e54de39951925f1d11b7f05b410
sofiarizzo04 <sofirizzo412@gmail.com>
2026-07-02 19:19:10 -0300

    cambios otra vez

--- guardar.php
+++ guardar.php
@@ -2,13 +2,14 @@
 
 include("conexion.php");
 
+$id = $_POST["id"];
 $nombre = $_POST["nombre"];
 $apellido = $_POST["apellido"];
 $correoelectronico = $_POST["correo electronico"];
 $sueldo = $_POST["sueldo"];
 
-$sql = "INSERT INTO login.empleados(nombre, apellido, correo electronico, sueldo)
-VALUES( '$nombre','$apellido','$correoelectronico','$sueldo')";
+$sql = "INSERT INTO login.empleados(id, nombre, apellido, correo electronico, sueldo)
+VALUES( '$id','$nombre','$apellido','$correoelectronico','$sueldo')";
 
 if($conexion->query($sql)){
     echo "Datos guardados correctamente";
